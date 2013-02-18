0f4f3e3a171dff0eb4375b84ef22e1341ad6ef51
Simon Lyngby Kokkendorff <10113573+silyko@users.noreply.github.com>
2013-02-18 09:53:10 +0100

    Updated doc - added license

--- Pak.py
+++ Pak.py
@@ -5,7 +5,7 @@ import os
 import sys
 import glob
 sys.argv.append("py2exe")
-extra_files=["icon.png"]
+extra_files=["icon.png","LICENCE.isc"]
 BIN=glob.glob(".\\bin\\*")
 COAST=glob.glob(".\\coast\\*world*")
 DOC=glob.glob(".\\doc\\*")
